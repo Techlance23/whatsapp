9386ccf471d9a3208c208a4b6f6492c6517fd4f7
sayooj <sayoojsunil2255@gmail.com>
2023-10-12 20:07:48 +0530

    initial4

---
+++ lib/src/feature/community/community_list_item_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/src/core/helper/datetime_helper.dart';
+import 'package:whatsapp/src/core/theme/text_theme.dart';
+
+class CommunityWidget extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final String image;
+  final DateTime lastChatTime;
+  final int unreadCount;
+
+
+  const CommunityWidget({super.key, required this.title, required this.subTitle, required this.image, required this.lastChatTime, required this.unreadCount});
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(image),
+        radius: 40,
+      ),
+
+      title: Text('$title', style: AppTextTheme.titleTextstyle,),
+      subtitle: Text('$subTitle', style: AppTextTheme.subtitleTextstyle,),
+      trailing: Column(children: [
+        Text(DateTimeHelper.getFormattedDate(lastChatTime),
+          style: TextStyle(
+              color: Colors.black54,
+              fontSize: 12
+          ),),
+        SizedBox(
+          width: 8,
+        ),
+
+
+
+        Visibility(
+            visible: unreadCount>0,
+            child: Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(50)
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("$unreadCount",style: TextStyle(
+                    color: Colors.white
+                ),),
+              ),
+            ))
+      ]),
+
+    );
+  }
+}
--- lib/src/feature/community/community_widget.dart
+++ lib/src/feature/community/community_widget.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/src/feature/community/community_list_item_widget.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Community');
+    return ListView(
+      children: [
+        CommunityWidget(title: "Cusat Updates", subTitle: "6282457856 added", image: "https://cusat.ac.in/images/campus/campus.jpg", lastChatTime: DateTime(2023,05,11), unreadCount: 2),
+        CommunityWidget(title: "Christ ", subTitle: "Results published", image: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Christ_College%2CIrinjalakuda.JPG", lastChatTime: DateTime(2023,05,12), unreadCount: 5)
+      ],
+    );
   }
 }
