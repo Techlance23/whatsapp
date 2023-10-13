6222b92070ba87299c566e1516e46b052c40d577
helana123 <helanajenson.3@gmail.com>
2023-10-13 17:45:41 +0530

    commit

---
+++ lib/src/pages/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final String title;
+  final List<Widget>? actions;
+  final Function()? onLeadingPressed;
+  final bool isLeadingBackButton;
+
+  CustomAppBar({
+    required this.title,
+    this.actions,
+    this.onLeadingPressed,
+    this.isLeadingBackButton = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: isLeadingBackButton
+          ? IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: onLeadingPressed ?? () => Navigator.pop(context),
+      )
+          : null,
+      title: Text(title),
+      actions: actions,
+    );
+  }
+}
