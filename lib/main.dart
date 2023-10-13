98760136944f6f9e7260d598775c066a6ba62c41
vinov02 <117145622+vinov02@users.noreply.github.com>
2023-10-13 22:36:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/src/feature/settings/whatsapp_settings.dart';
 import 'package:whatsapp/src/strings/strings.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
